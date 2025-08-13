b5ba513dd62eaf24d00738128b7ddf10638e86a1
sam <huapox@126.com>
2025-08-13 09:37:45 +0800

    build/buildx plat="--platform linux/ppc64le"
    build/buildx doBuildx v2501-alpine319-ppc64le
    
    buildx ali=$REPO_TEN_HK

--- build/buildx.sh
+++ build/buildx.sh
@@ -18,12 +18,14 @@ function doBuildx(){
     test ! -z "$REPO" && repo=$REPO #@gitac
     img="docker-libvirtd:$tag"
     # cache
-    ali="registry.cn-shenzhen.aliyuncs.com"
+    # ali="registry.cn-shenzhen.aliyuncs.com"
+    ali=$REPO_TEN_HK
     cimg="docker-libvirtd:$tag-cache" #tag-cache
     cache="--cache-from type=registry,ref=$ali/$ns/$cimg --cache-to type=registry,ref=$ali/$ns/$cimg"
     
     # plat="--platform linux/amd64,linux/arm64,linux/arm"
-    plat="--platform linux/amd64,linux/arm64" ##,linux/arm
+    # plat="--platform linux/amd64,linux/arm64" ##,linux/arm
+    plat="--platform linux/ppc64le"
 
     compile="alpine-compile";
     # test "$plat" != "--platform linux/amd64,linux/arm64,linux/arm" && compile="${compile}-dbg"
@@ -35,10 +37,6 @@ function doBuildx(){
     --build-arg NOCACHE=$(date +%Y-%m-%d_%H:%M:%S)
     """
 
-    # cd flux
-    # test "$plat" != "--platform linux/amd64,linux/arm64,linux/arm" && img="${img}-dbg"
-    # test "$plat" != "--platform linux/amd64,linux/arm64,linux/arm" && cimg="${cimg}-dbg"
-    cache="--cache-from type=registry,ref=$ali/$ns/$cimg --cache-to type=registry,ref=$ali/$ns/$cimg"
     output="--output type=image,name=$repo/$ns/$img,push=true,oci-mediatypes=true,annotation.author=sam"
     docker buildx build $cache $plat $args $output -f $dockerfile . 
 }
@@ -46,4 +44,4 @@ function doBuildx(){
 cd $cur/
 ns=infrastlabs
 ver=v51 #base-v5 base-v5-slim
-doBuildx v2501-alpine319 Dockerfile #latest> v2501
+doBuildx v2501-alpine319-ppc64le Dockerfile #latest> v2501
