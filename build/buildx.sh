55002b29e8eb3a8d06b2bb49e98de763e8eb5597
sam <huapox@126.com>
2025-08-15 10:54:36 +0800

    build/buildx doBuildx v2501-ubt2204 Dockerfile.ubt
    build/buildx doBuildx v2501-ubt2004 Dockerfile.ubt
    
    build/buildx dver=$3;
    
    build/buildx # doBuildx v2501-ubt2404 Dockerfile.ubt 24.04 &
    
    buildx 24
    
    buildx plat="--platform linux/amd64,linux/arm64,linux/ppc64le"
    
    build/buildx x11ubt
    
    build/buildx doBuildx v2501-ubt2204 Dockerfile.ubt
    
    build/buildx doBuildx v2501-ubt2004 Dockerfile.ubt
    
    build/buildx dver=$3;
    
    build/buildx # doBuildx v2501-ubt2404 Dockerfile.ubt 24.04 &
    
    buildx 24
    
    buildx plat="--platform linux/amd64,linux/arm64,linux/ppc64le"
    
    build/buildx doBuildx v2501-x11ubt2404 Dockerfile.x11base 24.04 &
    
    build/buildx plat="--platform linux/amd64,linux/arm64"
    
    build/buildx doBuildx v2501-x11ubt2404-dbg Dockerfile.x11base.dbg 24.04 &
    
    builx up
    
    buildx plat="--platform linux/amd64" #dbg
    
    build/buildx Dockerfile.x11base x3
    
    # Conflicts:
    #	build/buildx.sh

--- build/buildx.sh
+++ build/buildx.sh
@@ -12,33 +12,35 @@ echo "${DOCKER_REGISTRY_PW_dockerhub}" |docker login --username=${DOCKER_REGISTR
 function doBuildx(){
     local tag=$1
     local dockerfile=$2
+    local dver=$3;
 
     repo=registry-1.docker.io
     # repo=registry.cn-shenzhen.aliyuncs.com
     test ! -z "$REPO" && repo=$REPO #@gitac
     img="docker-libvirtd:$tag"
     # cache
-    ali="registry.cn-shenzhen.aliyuncs.com"
+    # ali="registry.cn-shenzhen.aliyuncs.com"
+    ali=$REPO_TEN_HK
     cimg="docker-libvirtd:$tag-cache" #tag-cache
     cache="--cache-from type=registry,ref=$ali/$ns/$cimg --cache-to type=registry,ref=$ali/$ns/$cimg"
     
     # plat="--platform linux/amd64,linux/arm64,linux/arm"
-    plat="--platform linux/amd64,linux/arm64" ##,linux/arm
+    # plat="--platform linux/amd64,linux/arm64,linux/ppc64le" ##+ppc64le; -linux/arm
+    plat="--platform linux/amd64,linux/arm64"
+    plat="--platform linux/amd64" #dbg
 
     compile="alpine-compile";
     # test "$plat" != "--platform linux/amd64,linux/arm64,linux/arm" && compile="${compile}-dbg"
     # --build-arg REPO=$repo/ #temp notes, just use dockerHub's
-    args="""
+    type=app; args="""
     --provenance=false 
     --build-arg REPO=$repo/
+    --build-arg TYPE=$type
+    --build-arg VER=$dver
     --build-arg COMPILE_IMG=$compile
     --build-arg NOCACHE=$(date +%Y-%m-%d_%H:%M:%S)
     """
 
-    # cd flux
-    # test "$plat" != "--platform linux/amd64,linux/arm64,linux/arm" && img="${img}-dbg"
-    # test "$plat" != "--platform linux/amd64,linux/arm64,linux/arm" && cimg="${cimg}-dbg"
-    cache="--cache-from type=registry,ref=$ali/$ns/$cimg --cache-to type=registry,ref=$ali/$ns/$cimg"
     output="--output type=image,name=$repo/$ns/$img,push=true,oci-mediatypes=true,annotation.author=sam"
     docker buildx build $cache $plat $args $output -f $dockerfile . 
 }
@@ -46,4 +48,9 @@ function doBuildx(){
 cd $cur/
 ns=infrastlabs
 ver=v51 #base-v5 base-v5-slim
-doBuildx v2501-alpine319 Dockerfile #latest> v2501
+# doBuildx v2501-alpine319 Dockerfile #latest> v2501
+# 
+doBuildx v2501-x11ubt2004 Dockerfile.x11ubt 20.04 &
+doBuildx v2501-x11ubt2204 Dockerfile.x11ubt 22.04 &
+doBuildx v2501-x11ubt2404 Dockerfile.x11ubt 24.04 &
+wait
